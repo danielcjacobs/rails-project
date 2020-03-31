7e92fc777464ac41d256bb79de66267dd3e4b7d2
Daniel Jacobs <danielcjacobs@cedarville.edu>
2020-03-31 00:30:19 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root 'plans#index'
+  resources :plans
+  devise_for :users, :controllers => {:registrations => 'registrations'}
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
